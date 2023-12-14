f554e2d35837aa912cdd5f2649e5771ff87ab8fb
Deema Essam <106381596+DeemaEssam@users.noreply.github.com>
2023-12-14 11:46:03 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -89,5 +89,4 @@ def export_to_excel(results, instructor_name):
     return file_path
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
